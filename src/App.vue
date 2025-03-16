8829caa0b9aee2681d64c300612948098a2f4df7
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2025-03-16 14:39:18 +0800

    fix testing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 import HelloWorld from './components/HelloWorld.vue'
 import { VueTree } from './index.ts'
 
+// import {VueTree} from '@tlylt/vue-tree'
+// import '@tlylt/vue-tree/style.css'
+
 </script>
 
 <template>
