ab32aeb700ec6d8603dacda7df99d6713dda18e5
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2025-03-16 14:30:11 +0800

    update export

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import VueTree from './components/VueTree.vue'
+import { VueTree } from './index.ts'
 
 </script>
 
@@ -15,18 +15,3 @@ import VueTree from './components/VueTree.vue'
     site.json
   </VueTree>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
