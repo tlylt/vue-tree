7059e38f51e4e3763cd238554d6915ace70fa08b
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2025-03-18 21:23:40 +0800

    minor update after prettier installation

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import VueTree from './components/VueTree.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import VueTree from "./components/VueTree.vue";
 
 // import { VueTree } from '@tlylt/vue-tree'
 // import '@tlylt/vue-tree/style.css'
-
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
   <VueTree>
-  C:/course/
-    textbook/
-    index.md
-    index.md
-    reading.md
-    site.json
+    C:/course/ 
+      textbook/
+      index.md
+      index.md
+      reading.md
+        site.json
   </VueTree>
-
 </template>
