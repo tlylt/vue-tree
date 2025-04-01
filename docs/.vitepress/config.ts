cd29bbfce802e9fc6eca722184bcb020ba565912
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2025-04-02 07:53:51 +0800

    test deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "@tlylt/vue-tree",
   description: "@tlylt/vue-tree",
+  base: '/vue-tree/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
